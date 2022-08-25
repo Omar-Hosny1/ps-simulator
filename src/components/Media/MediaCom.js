c1027244d592e1ca9000ea26b3d8f7939780aa63
Omar Hosny <honsyomar022@gmail.com>
2022-08-25 07:05:00 +0200

    Some Files Added

--- src/components/Media/MediaCom.js
+++ src/components/Media/MediaCom.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import Image from "./media.jpg";
+import MediaData from "./MediaData";
+import MediaPoster from "./MediaPoster";
 function MediaCom() {
-  return <div className="media"></div>;
+  const style = {
+    backgroundImage: `url("${Image}")`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  };
+  const MediaLogos = MediaData.map((ele) => (
+    <MediaPoster image={ele.image} key={Math.random()} />
+  ));
+  return (
+    <>
+      <div className="media" style={style}></div>
+      <div className="media-container">{MediaLogos}</div>
+      <div className="media-movie">
+        <h1>The Teriminal</h1>
+        <p>
+          The original Disney logo showed the profile of Mickey Mouse. When
+          animated, as the logo always was in the company's films and TV shows
+        </p>
+      </div>
+
+      <div className=""></div>
+    </>
+  );
 }
 
 export default MediaCom;
